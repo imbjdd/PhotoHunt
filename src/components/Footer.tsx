23c2702b336540a3586dbadd00f39927bd4d6382
imbjdd <b0bb1e@protonmail.com>
2025-02-25 14:42:11 +0100

    feat: add possibility to change pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { Tabbar } from '@telegram-apps/telegram-ui';
 import { useTranslations } from 'next-intl';
 
@@ -10,28 +12,38 @@ const tabs = [
   {
     id: 1,
     text: 'Home',
-    Icon: <Vote />
+    Icon: <Vote />,
+    url: ''
   },
   {
     id: 2,
     text: 'Camera',
-    Icon: <Camera />
+    Icon: <Camera />,
+    url: 'theme-params'
   },
   {
     id: 3,
     text: 'Ranking',
-    Icon: <Medal />
+    Icon: <Medal />,
+    url: 'theme-params'
   },
   {
     id: 4,
     text: 'Profile',
-    Icon: <CircleUser />
+    Icon: <CircleUser />,
+    url: 'theme-params'
   },
 ]
 
 export default function Footer() {
   const t = useTranslations('i18n');
   const [currentTab, setCurrentTab] = useState(tabs[0].id);
+  const router = useRouter();
+
+  function changePage(id:number) {
+    setCurrentTab(id)
+    router.push('/'+tabs.find(x => x.id === id)?.url)  
+  }
 
   return (
     <Tabbar>
@@ -39,7 +51,7 @@ export default function Footer() {
     id,
     text,
     Icon
-    }) => <Tabbar.Item key={id} text={text} selected={id === currentTab} onClick={() => setCurrentTab(id)}>
+    }) => <Tabbar.Item key={id} text={text} selected={id === currentTab} onClick={() => changePage(id)}>
         {Icon}
         </Tabbar.Item>)}
     </Tabbar>
